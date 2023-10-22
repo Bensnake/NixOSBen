b30f3bfc2dce2aad6e9a6da9a119ee77b7e05b98
Bensnake <64216808+Bensnake@users.noreply.github.com>
2023-10-22 14:21:27 +0300

    Update hyprpaper.nix
    Changes wallpaper file name

--- home-manager/features/hyprpaper.nix
+++ home-manager/features/hyprpaper.nix
@@ -2,7 +2,7 @@
 
 {
   home.file.".config/hypr/hyprpaper.conf".text = ''
-    preload = ~/Pictures/omer-tunc-sds-copy.jpg
-    wallpaper = DP-1,~/Pictures/omer-tunc-sds-copy.jpg
+    preload = ~/Pictures/omer-tunc-sds.jpg
+    wallpaper = DP-1,~/Pictures/omer-tunc-sds.jpg
   '';
 }
